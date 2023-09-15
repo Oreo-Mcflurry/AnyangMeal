04e564bdc065b90165870444cdc6cc02f110cafa
Musk <yuiop1029@naver.com>
2023-09-15 17:15:21 +0900

    Initial Commit

---
+++ AnyangMeal/AnyangMealApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnyangMealApp.swift
+//  AnyangMeal
+//
+//  Created by A_Mcflurry on 2023/09/15.
+//
+
+import SwiftUI
+
+@main
+struct AnyangMealApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
