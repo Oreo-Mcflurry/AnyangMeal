f88a2e6e3863667e9f3de8e3deea67091d89ceb7
유인호 <yuiop1029@naver.com>
2024-03-04 23:02:14 +0900

    Delete Comment

--- AnyangMeal/View/MainView/ViewModel/ViewModel.swift
+++ AnyangMeal/View/MainView/ViewModel/ViewModel.swift
@@ -12,43 +12,7 @@ class ViewModel: ObservableObject {
 	@Published var currentPage = 0
 
 	init() {
-//		fetchData()
-		// 더미 메뉴 데이터
-		let dummyMenuData: [String] = [
-			 "토마토 스파게티\n샐러드\n아이스티",
-			 "로제 파스타\n감자튀김\n과일",
-			 "그릴드 치킨\n콘샐러드\n레몬 에이드",
-			 "새우 볶음밥\n김치\n아이스 아메리카노",
-			 "스테이크\n감자그라탕\n바나나 스무디"
-		]
-
-		// 더미 좋아요 기기 데이터
-		let dummyLikeDevicesData: [[String]] = [
-			 ["iPhone X", "iPad Pro", "MacBook Pro"],
-			 ["iPhone 12", "Apple Watch Series 6", "Mac Mini"],
-			 ["iPad Air", "iMac", "iPhone SE"],
-			 ["MacBook Air", "iPhone 13", "Apple Watch SE"],
-			 ["Mac Pro", "iPad Mini", "iPhone 11"]
-		]
-
-		// 더미 날짜 데이터
-		let dateFormatter = DateFormatter()
-		dateFormatter.dateFormat = "yyyy-MM-dd"
-
-		let dummyDates: [Date] = [
-			 dateFormatter.date(from: "2024-03-01")!,
-			 dateFormatter.date(from: "2024-03-02")!,
-			 dateFormatter.date(from: "2024-03-03")!,
-			 dateFormatter.date(from: "2024-03-04")!,
-			 dateFormatter.date(from: "2024-03-05")!
-		]
-
-		// 더미 Meal 데이터 생성
-
-		for i in 0..<dummyMenuData.count {
-			 let meal = Meal(id: UUID().uuidString, meal: MealDetail(day: dummyDates[i], menu: dummyMenuData[i], likeDevice: dummyLikeDevicesData[i]))
-			meals?.data?.append(meal)
-		}
+		callRequest(.call)
 	}
 
 	func callRequest(_ kind: APIURLs) {
